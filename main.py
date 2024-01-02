4483e6023d181353d64d255ac568d3cdc82eef65
Marshal10 <marshalmaleane123@gmail.com>
2024-01-02 21:20:06 +0530

    Data Extract and check birthday

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import pandas as pd
+import datetime as dt
+
+data=pd.read_csv("birthdays.csv")
+
+#Data in dictionary format
+dict=data.to_dict(orient="records")
+
+now=dt.datetime.now()
+today_date=now.day
+today_month=now.month
 
 
-
-
+for person in dict:
+    if person["day"]==today_date and person["month"]==today_month:
+        print(f"Happy Birthday {person["name"]}")
