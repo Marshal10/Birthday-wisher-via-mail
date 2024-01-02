89f47857d8e7b619a7e58c5eb49aa98b4524931f
Marshal10 <marshalmaleane123@gmail.com>
2024-01-02 21:24:54 +0530

    Email functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import datetime as dt
 import random
+import smtplib
 
 data=pd.read_csv("birthdays.csv")
 
@@ -19,4 +20,16 @@ for person in dict:
         with open(f"letter_templates/letter_{random_letter_no}.txt") as file:
             data=file.read()
         letter=data.replace("[NAME]",f"{person["name"]}")
-        print(letter)
+        
+        my_email="YOUR EMAIL HERE"
+        #Refer the link on how to generate an app password https://www.youtube.com/watch?v=hXiPshHn9Pw
+        password="YOUR APP PASSWORD HERE"
+        
+        #Gmail: smtp.gmail.com , Hotmail: smtp.live.com, Outlook: outlook.office365.com, Yahoo: smtp.mail.yahoo.com
+        smtp_address="SMTP ADDRESS AS ABOVE"
+
+
+        with smtplib.SMTP(smtp_address) as connection:
+            connection.starttls()
+            connection.login(user=my_email,password=password)
+            connection.sendmail(from_addr=my_email,to_addrs=person["email"],msg=f"Subject:Happy Birthday\n\n{letter}")
